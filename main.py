1befca80f6f3a06c7bede159a70f7fbf898ad680
samuel <samuelaligator@gmail.com>
2023-08-17 11:58:17 +0200

    change pacman appearance according to direction and mouth in draw_pacman (line 73) & pacman_mouth (line 107) functions

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pyxel
 import numpy as np
 
+# In the grid, there are two columns before the start of the maze, to avoid teleportation problems.
+# So I have to move many elements two columns in my program.
+
 grid = np.array([  # The maze : 0 = empty square, 1 = wall, 2 = dot, 3 = large dot
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1],
@@ -43,12 +46,12 @@ direction_offset = {
 
 
 def draw_dots():
-    for column_number in range(2, grid.shape[1]):
+    for column_number in range(2, grid.shape[1]):  # 2, = two-column offset (see grid)
         for line_number in range(grid.shape[0]):
             if grid[line_number, column_number] == 2:
-                pyxel.rect(column_number * 8 + 3 - 16, line_number * 8 + 3, 2, 2, 2)
+                pyxel.rect(column_number * 8 + 3 - 16, line_number * 8 + 3, 2, 2, 2)  # -16 = two-column offset (see grid)
             elif grid[line_number, column_number] == 3:
-                pyxel.blt(column_number * 8 - 16, line_number * 8, 0, 224, 0, 8, 8, 0)
+                pyxel.blt(column_number * 8 - 16, line_number * 8, 0, 224, 0, 8, 8, 0)  # -16 = two-column offset (see grid)
             # to see the wall hitbox :
             # elif grid[line_number, column_number] == 1:
             #    pyxel.rect(column_number * 8 - 16, line_number * 8, 8, 8, 4)
@@ -64,22 +67,35 @@ class Pacman:
         self.input_direction = 2
         self.direction = 2
         self.walking = True
+        self.mouth = 2
+        self.i_mouth = 0
 
     def draw_pacman(self):
-        pyxel.blt(self.x - 2, self.y - 3, 1, 35, 1, 13, 13, 0)
+
+        # to center pacman
+        x = self.x - 2
+        y = self.y - 3
+
+        tile_x = {1: 19, 2: 3}
+        tile_y = {0: 49, 1: 33, 2: 17, 3: 1}
+
+        if self.mouth == 0:
+            pyxel.blt(x, y, 1, 35, 1, 13, 13, 0)
+        else:
+            pyxel.blt(x, y, 1, tile_x[self.mouth], tile_y[self.direction], 13, 13, 0)
 
     def main(self):
 
         self.pacman_direction()
+        self.pacman_mouth()
 
         if self.x % 8 == 0 and self.y % 8 == 0:
-            self.x_grid = self.x // 8 + 2
+            self.x_grid = self.x // 8 + 2  # +2 = two-column offset (see grid)
             self.y_grid = self.y // 8
             self.pacman_eats()
             self.check_walls()
 
         self.teleportation()
-
         self.pacman_movement()
 
     def pacman_direction(self):
@@ -87,6 +103,16 @@ class Pacman:
         for i in range(4):
             if pyxel.btn(direction[i]):
                 self.input_direction = i
+
+    def pacman_mouth(self):
+        converter = {0: 0, 1: 0, 2: 1, 3: 1, 4: 2, 5: 2, 6: 1, 7: 1}
+
+        if self.walking:
+            self.mouth = converter[self.i_mouth]
+            self.i_mouth = (self.i_mouth + 1) % 8
+        elif self.mouth == 0:
+            self.i_mouth = 2
+            self.mouth = 1
 
     def pacman_eats(self):
         grid[self.y_grid, self.x_grid] = 0
